106ca32fc350efcbc726f462a1473b4c11f236cd
tomaci <matias.code@gmail.com>
2017-08-12 13:11:32 -0300

    commit 0

--- auto.sh
+++ auto.sh
@@ -1,60 +1,60 @@
-git checkout -b brach-0
+git checkout brach-0
 echo 'batata 0' >> log.gol
 git add .
 git commit -a -m 'commit 0'
-git checkout -b brach-1
+git checkout brach-1
 echo 'batata 1' >> log.gol
 git add .
 git commit -a -m 'commit 1'
-git checkout -b brach-2
+git checkout brach-2
 echo 'batata 2' >> log.gol
 git add .
 git commit -a -m 'commit 2'
-git checkout -b brach-3
+git checkout brach-3
 echo 'batata 3' >> log.gol
 git add .
 git commit -a -m 'commit 3'
-git checkout -b brach-4
+git checkout brach-4
 echo 'batata 4' >> log.gol
 git add .
 git commit -a -m 'commit 4'
-git checkout -b brach-5
+git checkout brach-5
 echo 'batata 5' >> log.gol
 git add .
 git commit -a -m 'commit 5'
-git checkout -b brach-6
+git checkout brach-6
 echo 'batata 6' >> log.gol
 git add .
 git commit -a -m 'commit 6'
-git checkout -b brach-7
+git checkout brach-7
 echo 'batata 7' >> log.gol
 git add .
 git commit -a -m 'commit 7'
-git checkout -b brach-8
+git checkout brach-8
 echo 'batata 8' >> log.gol
 git add .
 git commit -a -m 'commit 8'
-git checkout -b brach-9
+git checkout brach-9
 echo 'batata 9' >> log.gol
 git add .
 git commit -a -m 'commit 9'
-git checkout -b brach-10
+git checkout brach-10
 echo 'batata 10' >> log.gol
 git add .
 git commit -a -m 'commit 10'
-git checkout -b brach-11
+git checkout brach-11
 echo 'batata 11' >> log.gol
 git add .
 git commit -a -m 'commit 11'
-git checkout -b brach-12
+git checkout brach-12
 echo 'batata 12' >> log.gol
 git add .
 git commit -a -m 'commit 12'
-git checkout -b brach-13
+git checkout brach-13
 echo 'batata 13' >> log.gol
 git add .
 git commit -a -m 'commit 13'
-git checkout -b brach-14
+git checkout brach-14
 echo 'batata 14' >> log.gol
 git add .
 git commit -a -m 'commit 14'
